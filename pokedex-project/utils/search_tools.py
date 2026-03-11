186558696e64f31c020dba0a7aa1edabb82bfe31
OceanMan441 <js15@kent.ac.uk>
2026-03-11 15:33:53 +0000

    Added a command that it does not matter what order the types are in when searching by type. Also added a team class and functionality to add/remove/show pokemon on the team. Lastly, updated the csv file to have the correct formatting for the new team functionality.

--- pokedex-project/utils/search_tools.py
+++ pokedex-project/utils/search_tools.py
@@ -14,3 +14,21 @@ def regex_search(pokemon_list, pattern):
             results.append(pokemon)
 
     return results
+
+def search_by_type(pokemon_list, search_type):
+    """
+    Searches the list of Pokémon by type(s), ignoring order, spaces, and case.
+    search_type: string like "Fire, Flying"
+    Returns a list of matching Pokémon.
+    """
+
+    search_set = set(t.strip().capitalize() for t in search_type.split(','))
+
+    results = []
+    for pokemon in pokemon_list:
+        # Convert Pokémon's type string into a set
+        pokemon_set = set(t.strip().capitalize() for t in pokemon.type.split(','))
+        if pokemon_set == search_set:
+            results.append(pokemon)
+
+    return results
